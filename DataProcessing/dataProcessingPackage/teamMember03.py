d3f4dfb63f3156c7633888cccb20b653a4cedd72
yemaneag <yemaneag@mail.uc.edu>
2025-04-10 13:13:57 -0400

    added car brand counter in teammember3.py

--- DataProcessing/dataProcessingPackage/teamMember03.py
+++ DataProcessing/dataProcessingPackage/teamMember03.py
@@ -1,6 +1,11 @@
 # teamMember03.py
-
+from collections import Counter
 class teamMember03:
     def print_something_interesting(self, data):
-        print("team member 03")
-         
+        try:
+            brand_counts = Counter(row['Brand'] for row in data)
+            print("Total amount of cars per brand:")
+            for brand, count in brand_counts.most_common(5):
+                print(f"{brand}: {count} listings")
+        except Exception as e:
+            print("teamMember03 Error:", e)
